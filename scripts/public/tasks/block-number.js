f59f5fd50431ef67946601bce94aa4227bcc065e
Akira <akira.dev.math@gmail.com>
2023-08-23 15:46:16 -0300

    Delete scripts/public directory

--- scripts/public/tasks/block-number.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("hardhat/config");
-(0, config_1.task)('block-number', 'Prints the current block number').setAction(async (taskArgs, hre) => {
-    const blockNumber = await hre.ethers.provider.getBlockNumber();
-    console.log(`Current block number: ${blockNumber}`);
-});
